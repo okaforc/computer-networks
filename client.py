e3ae4c735d8efba5d0c2fc008ee38f39a563670f
okaforc <75443890+okaforc@users.noreply.github.com>
2022-10-15 21:04:50 +0100

    starting assignment 1 part 2

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # based on https://pythontic.com/modules/socket/udp-client-server-example
 import socket
 import time
+import random
 
 # Headers
 
@@ -12,10 +13,13 @@ returned = b'RETURN '
 client_sig = b'C - '  # client signature
 greet = b'GREET '
 fetch = b'FETCH '
+start_req = b'START REQUEST'
+end_req = b'END REQUEST'
 received = b'RECEIVED '
 end = b'END'
 
 messages_to_request = []
+received_items = []
 
 
 def display_msg(msg, delay):
@@ -26,19 +30,31 @@ def display_msg(msg, delay):
 
 def add_item_to_request(item: str):
     """push an item onto the server request queue"""
-    messages_to_request.append(fetch + str.encode(item))
+    messages_to_request.append(str.encode(item))
 
 
 def remove_item_from_request(item: bytes):
     """pop the item from the server requests queue"""
-    [messages_to_request.remove(s) for s in messages_to_request if item in s]
+    messages_to_request.remove(item)
+    # try:
+    # except:
+    #     print("ERROR: NOT VALID ITEM")
 
 
-add_item_to_request("message1.txt")
-add_item_to_request("legally_downloaded_popular_movie.mp4")
-add_item_to_request("selfie.jpeg")
-add_item_to_request("legally_downloaded_popular_song.mp3")
+items_to_request = ["1.txt", "2.txt", "3.txt", "4.txt",
+                    "5.txt", "6.txt", "7.txt", "8.txt", "9.txt", "10.txt",
+                    "selfie.jpg", "high_quality.png", "ringtone.mp3", "cat.png",
+                    "cat_2.png", "cat_3.png", "groceries.txt", "dog.jpg"]
 
+
+# add a random amount of random files (5 files max)
+rand_n = random.randrange(1, 6)
+for i in range(rand_n):
+    add_item_to_request(
+        items_to_request[random.randrange(0, len(items_to_request))])
+
+
+len_of_items_requested = len(messages_to_request)
 
 serverAddressPort = ("", 20001)
 bufferSize = 1024
@@ -52,24 +68,40 @@ bytesToSend = msg1
 # Send to server using created UDP socket
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
+a = time.time()
+
 while True:
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
     message = msgFromServer[0]
     # only respond once a response from the server has been achieved
     if msgFromServer:
         display_msg(message, .5)
-        match message:
-            case b'S - Client Acknowledged' | b'S - Workers not ready' | b'S - READY' if len(messages_to_request) > 0:
+        if message == b'S - Client Acknowledged' or message == b'S - READY':
+            # send all the files in the queue
+            # bytesToSend = client_sig + start_req
+            # UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+            while len(messages_to_request) > 0:
+                msg = messages_to_request[0]
                 # server is awake, so ask to awaken workers
-                bytesToSend = client_sig + messages_to_request[0]
+                bytesToSend = client_sig + fetch + msg
+                # print(msg)
                 # Send to server using created UDP socket
                 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-            case _ if server_sig + returned in message:
-                ic = message[4:].split(b' ')[-1]  # incoming content
-                remove_item_from_request(ic) # pop received item from queue
-                bytesToSend = client_sig + received + ic
-                UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-            case _:
-                bytesToSend = client_sig + end
-                UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-                break
+                # ic = message[4:].split(b' ')[-1]  # incoming content
+                remove_item_from_request(msg)  # pop received item from queue
+            # bytesToSend = client_sig + end_req
+            # UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        elif server_sig + returned in message:
+            ic = message[4:].split(b' ')[1]  # incoming content
+            # remove_item_from_request(ic)  # pop received item from queue
+            bytesToSend = client_sig + received + ic
+            UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+            received_items.append(ic)
+        if len(messages_to_request) == 0 and len(received_items) == len_of_items_requested:
+            bytesToSend = client_sig + end
+            UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+            break
+
+b = time.time()
+print("############################\n" +
+      str(b - a) + " " + str(received_items) + "\n############################")
--- server.py
+++ server.py
@@ -13,17 +13,44 @@ bufferSize = 1024
 # Inwards
 client_sig = b"C - "  # client signature
 worker_sig = b"W - "  # worker signature
+greet = b'GREET'
+fetch = b'FETCH'
+start_req = b'START REQUEST'
+end_req = b'END REQUEST'
+received = b'RECEIVED'
+end = b'END'
 
 # Outwards
 ready = b'READY'
-end = b'END' 
+end = b'END'
 server_sig = b"S - "  # server signature
+
+bytecode = 0 # server header
 
 
 def display_msg(message, delay):
     """display the encoded message `msg` after `delay` seconds"""
     print("{}".format(message))
     time.sleep(delay)
+
+
+def key_from_value(d, val):
+    print(d)
+    print(val)
+    if val in d:
+        print(list(d.keys())[list(d.values()).index(val)])
+        return list(d.keys())[list(d.values()).index(val)]
+    return None
+
+
+def str_to_tuple(s: str):
+    """"Given a well-formed tuple string `s`, return it as a tuple.
+    `s` must be in the form \"(a.b.c.d, x)\", where `s[0]` is a string and
+    `s[1]` is an int, and a, b, c, d, x are ints"""
+    p = s.replace("(", "").replace(")", "").strip().split(",")
+    a = p[0][1:-1]
+    b = int(p[1])
+    return (a, b)
 
 
 # Create a datagram socket
@@ -33,19 +60,69 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
-shutdowns = 0
+client_shutdowns = 0
+worker_shutdowns = 0
 
 workerIPs = []
 usedWorkerIPs = []
-clientIP = []
+clients = {}
+
+requests = []
+is_receiving = False
+curr_add = 0
+
+
+def is_backlog_empty():
+    for i in clients:
+        if len(clients[i]) > 0:
+            return False
+    return True
+
+
+def send_client_content(address, content):
+    """Sends the `address`' message (`content`) to any available workers
+
+    Args:
+        address (tuple): address that is requesting the item
+        content (byte): the content of the address' message
+    """
+
+    if len(workerIPs) > 0:
+        cont = None
+        if type(content) == list:
+            cont = b' '.join(content)
+        else:
+            cont = content
+        # if there are any free workers available,
+        # workers are available
+        bytesToSend = server_sig + cont + b' | ' + str.encode(str(address))
+
+        # Sending a reply to chosen worker
+        print(len(workerIPs))
+        worker_chosen = random.randrange(0, len(workerIPs))
+        UDPServerSocket.sendto(bytesToSend, workerIPs[worker_chosen])
+
+        # mark the chosen worker as used
+        usedWorkerIPs.append(workerIPs[worker_chosen])
+        workerIPs.remove(workerIPs[worker_chosen])
+
 
 # Listen for incoming datagrams
 while True:
     time.sleep(.5)
+
+    # if there are still items left to send, send them to the workers
+    if not is_backlog_empty():
+        for i in clients:
+            for j in clients[i]:
+                # print(j)
+                if len(workerIPs) > 0:
+                    send_client_content(i, j)
+                    clients[i].remove(j)
+
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
-
     # signature of received message.
     # the address is saved for each message
     rec_sig = message[:4]
@@ -54,69 +131,73 @@ while True:
     # Each message is prefixed with the signature of the origin, including the messages the server sends out.
     content = message[4:].split(b' ')
     action = content[0]
+
     if rec_sig == client_sig:
+        display_msg(message, .5)
         # what is the client asking to do?
-        match action:
-            case b'GREET':
-                display_msg(message, .5)
-                clientIP.append(address)
-                msgFromServer = "Client Acknowledged"
-                bytesToSend = server_sig + str.encode(msgFromServer)
-                # Sending a reply to client
-                UDPServerSocket.sendto(bytesToSend, address)
-            case b'FETCH':
-                display_msg(message, .5)
-                # if there are any free workers available,
-                if len(workerIPs) > 0:
-                    # workers are available
-                    bytesToSend = server_sig + b" ".join(content)
-
-                    # Sending a reply to chosen worker
-                    worker_chosen = random.randrange(0, len(workerIPs))
-                    UDPServerSocket.sendto(bytesToSend, workerIPs[worker_chosen])
-
-                    # mark the chosen worker as used
-                    usedWorkerIPs.append(workerIPs[worker_chosen])
-                    workerIPs.remove(workerIPs[worker_chosen])
-                else:
-                    msgFromServer = "Workers not ready"
-                    bytesToSend = server_sig + str.encode(msgFromServer)
-
-                    # Sending a reply to client
-                    UDPServerSocket.sendto(bytesToSend, address)
-            case b'RECEIVED':
-                display_msg(message, .5)
-                bytesToSend = server_sig + ready
-                UDPServerSocket.sendto(bytesToSend, address)
-            case b'END':
-                display_msg(message, .5)
+        # match action:
+        if action == greet:
+            clients[address] = []
+            msgFromServer = "Client Acknowledged"
+            bytesToSend = server_sig + str.encode(msgFromServer)
+            # Sending a reply to client
+            UDPServerSocket.sendto(bytesToSend, address)
+        elif action == start_req:
+            is_receiving = True
+            curr_add = address
+        elif action == fetch:
+            is_receiving = False
+            if len(workerIPs) > 0:
+                send_client_content(address, content)
+            else:
+                # send to client backlog
+                # print(clients[address])
+                clients[address].append(b" ".join(content))
+        elif action == received:
+            bytesToSend = server_sig + ready
+            UDPServerSocket.sendto(bytesToSend, address)
+        elif action == end:
+            client_shutdowns += 1
+            if client_shutdowns == len(clients):
                 bytesToSend = server_sig + end
-                [UDPServerSocket.sendto(bytesToSend, x) for x in workerIPs] # shut down all workers
-            case _:
-                bytesToSend = server_sig + str.encode("ERROR Unknown Requested Action: ") + message
-                UDPServerSocket.sendto(bytesToSend, address)
+                [UDPServerSocket.sendto(bytesToSend, x) for x in workerIPs]  # shut down all workers
+        else:
+            if is_receiving:
+                clients[curr_add].append(b" ".join(content[1:]))
+            bytesToSend = server_sig + \
+                str.encode("ERROR Unknown Requested Action: ") + message
+            UDPServerSocket.sendto(bytesToSend, address)
     elif rec_sig == worker_sig:
+        display_msg(message, .5)
         match action:
             case b'GREET':
                 # There shouldn't be any duplicate addresses, but the check is just in case
                 if address not in workerIPs:
                     # add all new worker IP addresses.
                     workerIPs.append(address)
-                    display_msg(message, .5)
+                    # display_msg(message, .5)
                     msg = "Worker Acknowledged"
                     bytesToSend = server_sig + str.encode(msg)
                     UDPServerSocket.sendto(bytesToSend, address)
             case b'RETURN':
-                display_msg(message, .5)
+                # display_msg(message, .5)
                 # free the current worker IP address
                 usedWorkerIPs.remove(address)
                 workerIPs.append(address)
                 bytesToSend = server_sig + b" ".join(content)
-                UDPServerSocket.sendto(bytesToSend, clientIP[0])
+                #  + b' ' + str.encode(str(address[1]))
+                # get backlog client's address
+                ad = str_to_tuple(bytesToSend.split(b" | ")[-1].decode())
+                UDPServerSocket.sendto(
+                    bytesToSend + b' ' + str.encode(str(address[1])), ad)
+                # remove item from client backlog
+                mes = fetch + b' ' + content[1]
+                if mes in clients[ad]:
+                    clients[ad].remove(fetch + b' ' + content[1])
             case b'END':
-                display_msg(message, .1)
-                shutdowns += 1
-                if shutdowns == len(workerIPs):
+                # display_msg(message, .1)
+                worker_shutdowns += 1
+                if worker_shutdowns == len(workerIPs):
                     print(server_sig + b'Goodbye')
                     break
     else:
